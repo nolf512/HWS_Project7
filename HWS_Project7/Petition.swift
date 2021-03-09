b56e00ea668e96ab5e02288430a5f4cbed400767
nofprog <k.jin3217@gmail.com>
2021-03-09 23:39:40 +0900

    TableView構築, jsonデータ取得

---
+++ HWS_Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  HWS_Project7
+//
+//  Created by J on 2021/03/09.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ HWS_Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  HWS_Project7
+//
+//  Created by J on 2021/03/09.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
