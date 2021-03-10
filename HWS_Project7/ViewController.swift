d041a80d155b5dafbfdcc41b01fecf0d9cedf109
nofprog <k.jin3217@gmail.com>
2021-03-10 22:37:53 +0900

    エラー処理, AlertAction

--- HWS_Project7/ViewController.swift
+++ HWS_Project7/ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 0 {
+           urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {
             
@@ -23,12 +28,23 @@ class ViewController: UITableViewController {
                 
                 //データ取得
                 parse(json: data)
+                
+            } else {
+                showError()
             }
+        } else {
+            showError()
         }
-        
-        
-        
+           
     }
+    
+    
+    func showError(){
+        let ac = UIAlertController(title: "Error", message: "Loading Error: Check Your Connection", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
     
     func parse(json: Data){
         let decoder = JSONDecoder()
